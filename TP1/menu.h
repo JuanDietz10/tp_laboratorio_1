00e6e66e1bbbf8ba3c9753bedf278aafa88c4eba
juandietz <52494265+juandietz@users.noreply.github.com>
2019-09-05 22:12:08 -0300

    TP1

---
+++ TP1/menu.h
@@ -0,0 +1,9 @@
+
+// Prototipos
+
+#ifndef MENU_H
+#define MENU_H
+
+void menuOptions(void);
+
+#endif
---
+++ TP1/strings_esp.h
@@ -0,0 +1,37 @@
+/*
+En este archivo se definen las cadenas que se usan para dan un mensaje al usuario, y no tienen ningún uso en la lógica del programa
+*/
+
+#ifndef STRINGS_ESP_H
+#define STRINGS_ESP_H
+
+#define MSG_INGRESO_VALOR_A         " Ingrese valor de A (Dominio: Enteros)"
+#define MSG_INGRESO_VALOR_B         " Ingrese valor de B (Dominio: Enteros)"
+#define MSG_INGRESO_INVALIDO        " Caracter invalido"
+#define MSG_OPERACION_INVALIDA      " Operacion invalida, vuelva a intentarlo"
+#define MSG_ERROR_DIVISION_CERO     " No se puede dividir por cero"
+#define MSG_ERROR_FACT_NEGATIVO     " No se puede calcular el factorial de un numero negativo"
+#define MSG_SALUDO_INICIO           " Bienvenido"
+#define MSG_SALUDO_FINAL            " Muchas gracias, hasta pronto."
+#define MSG_CONFIRMACION_SALIR      " Desea salir?	  si (S)  /  no (n)"
+
+#define LINEA                       "----------------------------------------"
+#define MSG_CODIGO                  "  Opciones                      Codigo"
+#define MSG_SUMA                    ". Calcular la suma (A+B)           (+)"
+#define MSG_RESTA                   ". Calcular la resta (A-B)          (-)"
+#define MSG_DIVISION                ". Calcular la division (A/B)       (/)"
+#define MSG_MULTIPLICACION          ". Calcular la multiplicacion (A*B) (*)"
+#define MSG_FACTORIAL_A             ". Calcular el factorial A!         (a)"
+#define MSG_FACTORIAL_B             ". Calcular el factorial B!         (b)"
+#define MSG_CAMBIAR_VALORES         ". Cambiar valores                  (c)"
+#define MSG_SALIR                   ". Salir                            (S)"
+#define MSG_INGRESO_OPERACION       "  Ingrese operacion:                "
+
+#define MSG_IMPRIMIR_SUMA           "  El resultado de A+B es: "
+#define MSG_IMPRIMIR_RESTA          "  El resultado de A-B es: "
+#define MSG_IMPRIMIR_MULTIPLICACION "  El resultado de A*B es: "
+#define MSG_IMPRIMIR_DIVISION       "  El resultado de A/B es: "
+#define MSG_IMPRIMIR_FACTORIAL_A    "  El resultado de A! es: "
+#define MSG_IMPRIMIR_FACTORIAL_B    "  El resultado de B! es: "
+
+#endif
