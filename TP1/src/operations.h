165cb9fd6a88515969cd3b0740a806f15369fe58
Juan Dietz <juanedietz@gmail.com>
2020-09-07 03:42:01 -0300

    release 07092020

---
+++ TP1/src/operations.h
@@ -0,0 +1,13 @@
+
+// Prototipos
+
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+int   add       (int a, int b);
+int   subtract  (int a, int b);
+int   multiply  (int a, int b);
+int   divide    (int a, int b, float* pResultado);
+int   factorial (int n);
+
+#endif
